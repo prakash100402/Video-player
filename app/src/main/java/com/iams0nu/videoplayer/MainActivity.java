e8b3d7b4b4cc5d827d97db0db9a4873d1967a02e
prakash100402 <avenjer2002@gmail.com>
2023-01-09 01:03:47 +0530

    Video player by Prakash kumar sah

---
+++ app/src/main/java/com/iams0nu/videoplayer/MainActivity.java
@@ -0,0 +1,24 @@
+package com.iams0nu.videoplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.vdo);//add mp4 file in raw directory before running the app.
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+
+    }
+}
